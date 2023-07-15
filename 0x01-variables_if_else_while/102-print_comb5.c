fc0cd7539f550a20f8ac994ffa2c7d205444b05e
ahmedalaqib <ahmedalaqib@gmail.com>
2023-07-14 19:51:37 -0700

    commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+	int p;
+	int q;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (m = 0; m <= 9; m++)
+		{
+			for (p = 0; p <= 9; p++)
+			{
+				for (q = 0; q <= 9; q++)
+				{
+					if ((n < p) || (n == p && m < q))
+					{
+						putchar(n + '0');
+						putchar(m + '0');
+						putchar(' ');
+						putchar(p + '0');
+						putchar(q + '0');
+						if (!(n == 9 && m == 8))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
