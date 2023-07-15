f25b2ab108783e1f2e703e4d29ebfad45c204231
ahmedalaqib <ahmedalaqib@gmail.com>
2023-07-14 17:54:44 -0700

    commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in order
+ * Return: 0
+ */
+
+int main(void)
+{
+	for(char a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
