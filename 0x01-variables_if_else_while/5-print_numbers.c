c4c3d4907f0c4ed9320cfce04a2aa433c056ca17
ahmedalaqib <ahmedalaqib@gmail.com>
2023-07-14 18:23:32 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10
+ *	 starting from 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		printf("%i", n);
+	putchar('\n');
+
+	return (0);
+}
