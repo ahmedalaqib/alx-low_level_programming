efd5b91afbdd3468baa83366d440683bd586e205
ahmedalaqib <ahmedalaqib@gmail.com>
2023-07-18 18:49:31 -0700

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -25,9 +25,9 @@ void print_to_98(int n)
 		for (i = n; i >= 98; i--)
 		{
 			if (i != 98)
-                                printf("%d, ", i);
-                        else
-                                printf("%d\n", i);
+				printf("%d, ", i);
+			else
+				printf("%d\n", i);
 		}
 	}
 }
