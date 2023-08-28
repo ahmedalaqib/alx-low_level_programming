4a4c51761a6a62162ef9291ed63a07723cacf446
ahmedalaqib <ahmedalaqib@gmail.com>
2023-08-28 21:23:10 +0000

    check failure

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 /**
- *  * malloc_checked - Allocate memory using malloc
- *   * @b: The number of bytes to be allocated
- *    *
- *     * Return: A pointer to the allocated memory,
- *      *	   if malloc fails, the function should cause normal process
- *       *	   termination with a status value of 98
- *        */
+ * malloc_checked - Allocate memory using malloc
+ * @b: The number of bytes to be allocated
+ *
+ * Return: A pointer to the allocated memory,
+ *	 if malloc fails, the function should cause normal process
+ *	 termination with a status value of 98
+ **/
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
