eed18cd7d925e999297b6e75a807e20f1f40c942
ahmedalaqib <ahmedalaqib@gmail.com>
2023-09-04 18:28:35 +0000

    commit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - Create a text file
+ * @filename: The name of the file to create
+ * @text_content: A NULL terminated string to write to the file
+ * Return: 1 - On success
+ *	 -1 - On failure (If filename is NULL,file can not be created,
+ *	 file can not be written,write “fails”, etc…)
+ **/
+int create_file(const char *filename, char *text_content)
+{
+	int op, wr, len = 0;
+
+	if (!filename)
+		return (-1);
+
+	while (text_content && text_content[len])
+		len++;
+
+	op = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
+	wr = write(o, text_content, len);
+
+	if (op == -1 || wr == -1)
+		return (-1);
+
+	close(op);
+	return (1);
+}
